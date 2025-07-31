8124b0d8771ed74c51784ec6d99d97158f6d5fe9
Brayan-Camargo <brayancamargoramirez@gmail.com>
2025-07-30 21:46:03 -0600

    creación de array y funcion para agregar a lista los nombres de amigos, limpiar el espacio y finalmente mostrar una alerta que nos diga que no se agrego ningun fato o que no es valido

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    //creamos variable para obtenes los datos y despues agregarlos con push a nuestra cadena
+    let addAmigo = document.getElementById("amigo").value;
+
+    //agregamos amigos a lista, limpiamos pantalla y mandamos alert
+    if (addAmigo != "") {
+        amigos.push(addAmigo);
+        document.getElementById("amigo").value = "";
+        console.log(addAmigo);
+        console.log(amigos);
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+}
