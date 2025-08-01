0bc200eaf8e2b7e62db3954bd696f4c51347486f
Brayan-Camargo <brayancamargoramirez@gmail.com>
2025-07-31 19:49:07 -0600

    Se arreglo un detalle en la guncion agregarAmigo y se creo toda la funcion lista para mostrar el nombre de la lista ya creada con un bucle for, nota: se púede mejorar y simplificar con la funcion foreach, pero por metodos de aprendizaje dejamos esta funcion

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -3,21 +3,31 @@ let amigos = [];
 
 function agregarAmigo() {
     //creamos variable para obtenes los datos y despues agregarlos con push a nuestra cadena
-    let addAmigo = document.getElementById("amigo").value;
+    let addAmigo = document.getElementById("amigo").value.trim();
 
     //agregamos amigos a lista, limpiamos pantalla y mandamos alert
-    if (amigos === "") {
+    if (addAmigo === "") {
         alert("Por favor, inserta un nombre.");
     } else if(amigos.includes(addAmigo)) {
+        document.getElementById("amigo").value = "";
         alert("Ese nombre ya fue agregado, ingresa un nombre nuevo.");
     } else {
-    amigos.push(addAmigo);
+        amigos.push(addAmigo);
         document.getElementById("amigo").value = "";
         console.log(addAmigo);
         console.log(amigos);
+        lista();
     }
 }
 
-function listaAmigos() {
+function lista() {
+    //Haremos que la lista se muestre en la pagina y se limpie al final
+    let mostrarLista = document.getElementById("listaAmigos");
+    mostrarLista.innerHTML = "";
 
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        mostrarLista.appendChild(li);
+    }
 }
